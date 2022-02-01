f0281803bbc56cc34ae87a68d14660eef492f00a
RAMIL ARSAEV <gattoramm@gmail.com>
2022-02-01 15:38:43 +0300

    Update HighArray.java

--- Lafore-Book-Tasks-Java/src/Part02_Arrays/Tasks/Task2_3/HighArray.java
+++ Lafore-Book-Tasks-Java/src/Part02_Arrays/Tasks/Task2_3/HighArray.java
@@ -1,10 +1,11 @@
 package Part02_Arrays.Tasks.Task2_3;
 
-// Класс массива с высокоуровневым интерфейсом
-// Добавить метод, возвращающий наибольшее значение ключа или -1, если массив пуст
-// Элемент с наибольшим ключем должен удаляться из массива
-// Реализовать алгоритм сортировки, который не изменяет класса HighArray (а изменяет только код main())
-
+/**
+ * Класс массива с высокоуровневым интерфейсом
+ * Добавить метод, возвращающий наибольшее значение ключа или -1, если массив пуст
+ * Элемент с наибольшим ключем должен удаляться из массива
+ * Реализовать алгоритм сортировки, который не изменяет класса HighArray (а изменяет только код main())
+ */
 class HighArray {
     private long[] a;
     private int nElems;
@@ -16,10 +17,10 @@ class HighArray {
 
     public boolean find(long searchKey) {
         int j;
-        for(j=0; j<nElems; j++)
-            if(a[j] == searchKey)
+        for (j=0; j<nElems; j++)
+            if (a[j] == searchKey)
                 break;
-        if(j == nElems)
+        if (j == nElems)
             return false;
         else
             return true;
@@ -32,13 +33,13 @@ class HighArray {
 
     public boolean delete(long value) {
         int j;
-        for(j=0; j<nElems; j++)
-            if(value == a[j])
+        for (j=0; j<nElems; j++)
+            if (value == a[j])
                 break;
-        if(j == nElems)
+        if (j == nElems)
             return false;
         else {
-            for(int k=j; k<nElems; k++)
+            for (int k=j; k<nElems; k++)
                 a[k] = a[k+1];
             nElems--;
             return true;
@@ -46,7 +47,7 @@ class HighArray {
     }
 
     public void display() {
-        for(int j=0; j<nElems; j++)
+        for (int j=0; j<nElems; j++)
             System.out.print(a[j] + " ");
         System.out.println("");
     }
@@ -54,8 +55,8 @@ class HighArray {
     public long removeMax() {
         long max = -1;
 
-        for(int j=0; j<nElems; j++)
-            if(a[j] > max)
+        for (int j=0; j<nElems; j++)
+            if (a[j] > max)
                 max = a[j];
 
         delete(max);
